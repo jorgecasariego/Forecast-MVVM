4ac7f089622710acbb5f2dfe1a5f441bf599f2dd
Jorge Casariego <jorgekasa@gmail.com>
2019-03-05 16:57:39 -0300

    Navigation between fragments configured

---
+++ app/src/main/java/com/enterprisesolutions/forecastmvvm/ui/settings/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.enterprisesolutions.forecastmvvm.ui.settings
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+
+    }
+
+}
